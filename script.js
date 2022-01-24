d5aac0a45cec59898d5387fc237566b49bc3e782
himanshuHeliverse <himasnhu@heliverse>
2022-01-24 16:09:29 +0530

    resend btn

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 $(document).ready(function () {
 
 
-    // var phoneValue = 0;
-    // var name = ""
-    // var consultation = ""
-    // var form = document.getElementById("form");
-    // form.addEventListener("submit", e => {
-    //     e.preventDefault();
-    //     $("#popup").show()
-    //     let url = 'https://onecare.co.in/wp-json/api/v1/send-otp'
+    var phoneValue = 0;
+    var name = ""
+    var consultation = ""
+    var form = document.getElementById("form");
 
-    //     phoneValue = e.target.phone.value
-    //     name = e.target.name.value
-    //     consultation = e.target.consultation.value
-    //     fetch(url, {
-    //         headers: {
-    //             "Content-Type": "Application/json"
-    //         },
-    //         method: 'POST',
-    //         mode: "cors",
-    //         body: JSON.stringify({
-    //             phone: phoneValue
+    $("#resend").click(function(){
 
-    //         })
-    //     })
-    //         .then(resp => console.log(resp))
-    //         .catch(err => console.log(err))
+        otpCall()
+    })
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        $("#popup").show()
+        phoneValue = e.target.phone.value
+        name = e.target.name.value
+        consultation = e.target.consultation.value
+        otpCall()
+
+    });
 
 
+function otpCall(){
+    let url = 'https://onecare.co.in/wp-json/api/v1/send-otp'
 
-    // });
+ 
+    fetch(url, {
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        method: 'POST',
+        mode: "cors",
+        body: JSON.stringify({
+            phone: phoneValue
 
+        })
+    })
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
 
-
+}
 
     // var popup = document.getElementById("popup-form");
     // popup.addEventListener("submit", e => {
